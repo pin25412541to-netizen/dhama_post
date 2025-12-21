a0a0f7f3eca697fcc4404b3807afa2e843087340
pin25412541to-netizen <pin25412541to@gmail.com>
2025-12-21 21:10:06 +0700

    ีuse firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCqZ9ektyzgI7zlwbGEHWohplXSqsR_kdg",
+  authDomain: "dhama-1095f.firebaseapp.com",
+  projectId: "dhama-1095f",
+  storageBucket: "dhama-1095f.firebasestorage.app",
+  messagingSenderId: "377776492558",
+  appId: "1:377776492558:web:17e6b714183bef98b36c5f",
+  measurementId: "G-7ER6Z9WTJE"
+};
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
   ]
 };
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -2,15 +2,17 @@ import { Component, inject, signal } from '@angular/core';
 import { Post, ReadingPost } from '../../model/post';
 import { PostService } from '../../services/post/postService';
 import { ReadingPostService } from '../../services/readingPost/reading-post';
+import { first, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 interface Menu {
-  name: string
-  link: string
+  name: string;
+  link: string;
 }
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -18,30 +20,43 @@ export class Home {
   postService = inject(PostService);
   readingPostService = inject(ReadingPostService);
 
-  posts = signal<Post[]>(this.postService.getAllPosts());
-  readingPost = signal<ReadingPost[]>(this.readingPostService.getAllPosts());
-  title = "บันทึกธรรม"
+  posts = signal<Observable<any[]>>(this.postService.getAllPosts());
+  readingPost = signal<Observable<any[]>>(this.readingPostService.getAllPosts());
+  title = 'บันทึกธรรม';
   menu: Menu[] = [
     {
-      name: "ธรรมะสั้นๆ",
-      link: "",
+      name: 'ธรรมะสั้นๆ',
+      link: '',
     },
     {
-      name: "ธรรมะบรรยาย",
-      link: ""
-    }
-  ]
-  menuActive = signal<string>(this.menu[0].name)
-  isModalOpen = signal(false)
-  readPostActive = signal<ReadingPost>(this.readingPost()[0])
+      name: 'ธรรมะบรรยาย',
+      link: '',
+    },
+  ];
+  menuActive = signal<string>(this.menu[0].name);
+  isModalOpen = signal(false);
+  readPostActive = signal<ReadingPost>({
+    title: '',
+    message: '',
+  });
 
-  closeModal = ()=>{
-    this.isModalOpen.set(false)
+  constructor() {
+    this.readingPost()
+      .pipe(first())
+      .subscribe((rs) => {
+        this.readPostActive.set(rs[0] as ReadingPost);
+      });
   }
 
-  clickReadPost = (index: number)=>{
-    this.readPostActive.set(this.readingPost()[index])
-    this.isModalOpen.set(true)
-  }
-  
+  closeModal = () => {
+    this.isModalOpen.set(false);
+  };
+
+  clickReadPost = (title: string, message: string) => {
+    this.readPostActive.set({
+      title: title,
+      message: message,
+    });
+    this.isModalOpen.set(true);
+  };
 }
